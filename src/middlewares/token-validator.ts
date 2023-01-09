6a7d88b5b945e0017ec75d9aa39e31b7b610188c
Fauzie <fauzie811@yahoo.com>
2023-01-09 09:11:20 +0300

    update token validator

--- src/middlewares/token-validator.ts
+++ src/middlewares/token-validator.ts
@@ -1,8 +1,17 @@
-import type { RequestHandler } from 'express';
+import type { Request, RequestHandler } from 'express';
+
+function extractToken(req: Request) {
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    return req.headers.authorization.split(' ')[1];
+  } else if (req.query && req.query.token) {
+    return req.query.token;
+  }
+  return null;
+}
 
 const validate: RequestHandler = (req, res, next) => {
-  if (req.query.token !== req.app.get('token'))
-    return res.status(401).json({ error: 'Invalid token' });
+  const token = extractToken(req);
+  if (token !== req.app.get('token')) return res.status(401).json({ error: 'Invalid token' });
   next();
 };
 
